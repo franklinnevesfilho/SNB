19bc600e6c846f137f644c1d9b2cbd17d384cd4f
franklinnevesfilho <franklin.neves.filho@gmail.com>
2023-08-25 00:39:37 -0400

    reformat

--- snb.backend/src/main/java/com/four/snb/SnbApplication.java
+++ snb.backend/src/main/java/com/four/snb/SnbApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SnbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SnbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SnbApplication.class, args);
+    }
 
 }
--- snb.backend/src/test/java/com/four/snb/SnbApplicationTests.java
+++ snb.backend/src/test/java/com/four/snb/SnbApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SnbApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
