04525ae6c8baa0f428c2c6ed5329f6dc48fcf57f
Franklin Neves Filho <88294924+franklinnevesfilho@users.noreply.github.com>
2023-08-25 00:18:37 -0400

    Added front end folder as well as backend folder
    Signed-off-by: franklinnevesfilho <franklin.neves.filho@gmail.com>

---
+++ snb.backend/src/main/java/com/four/snb/SnbApplication.java
@@ -0,0 +1,13 @@
+package com.four.snb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnbApplication.class, args);
+	}
+
+}
---
+++ snb.backend/src/test/java/com/four/snb/SnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.four.snb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
