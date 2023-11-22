8787046613226ae4f8feb8f08936e4e0e96dac46
Luke Slautterback <lukeslautterback@gmail.com>
2023-11-22 18:43:54 -0500

    Update LoginPage
    Now stores token in user cookies

--- Luke/src/pages/LoginPage.jsx
+++ Luke/src/pages/LoginPage.jsx
@@ -1,81 +1,107 @@
-import React, {useState} from 'react';
-import LoginForm from "../components/LoginForm.jsx";
+import React, { useState } from 'react';
+import LoginForm from '../components/LoginForm.jsx';
 
-function LoginPage({apiCall}) {
+function LoginPage({ apiCall }) {
+  const [path, setPath] = useState('');
+  const [hasAccount, setHasAccount] = useState(true);
+  const [loginInputs, setLoginInputs] = useState({
+    email: '',
+    password: '',
+  });
+  const [registerInputs, setRegisterInputs] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
 
-    const [path, setPath] = useState('');
-    const [hasAccount, setHasAccount] = useState(true)
-    const [loginInputs, setLoginInputs] = useState({
-        email:'',
-        password:''
-    })
-    const [registerInputs, setRegisterInputs] = useState({
-        firstName:'',
-        lastName:'',
-        email:'',
-        password:''
-    })
+  const handleLogin = async () => {
+    setPath('/auth/login');
 
-    const loginRequest = () =>{
-        setPath('/auth/login')
-        apiCall(
-            path,
-            'POST',
-            {'Content-Type': 'application/json'},
-            loginInputs,
-            (r) => console.log(r)
-        );
-    }
-    const registrationRequest = () =>{
-        apiCall(
-            '/auth/register',
-            'POST',
-            {'Content-Type': 'application/json'},
-            registerInputs,
-            (r) => console.log(r)
-        )
-
-    }
-
-    return (
-        <>
-            <h1>LoginPage</h1>
-            <div className="form-container">
-                <div className="login-btn-container">
-                    <button className={'btn'} onClick={()=> setHasAccount(true)}>Log in</button>
-                    <button className={'btn'} onClick={()=> setHasAccount(false)}>Register</button>
-                </div>
-
-                { hasAccount ?
-                    <LoginForm
-                        inputs={loginInputs}
-                        handleChange={(e) =>
-                            setLoginInputs((prevState) => ({
-                                ...prevState,
-                                [e.target.name]: e.target.value.trim(),
-                            }))
-                        }
-                        onSubmit={loginRequest}
-                        submitType='Login'
-                    />
-
-                    :
-
-                    <LoginForm
-                        inputs={registerInputs}
-                        handleChange={(e) =>
-                            setRegisterInputs((prevState) => ({
-                                ...prevState,
-                                [e.target.name]: e.target.value.trim(),
-                            }))
-                        }
-                        onSubmit={registrationRequest}
-                        submitType='Registration'
-                    />
-                }
-            </div>
-        </>
+    // Make the login API call
+    apiCall(
+      path,
+      'POST',
+      { 'Content-Type': 'application/json' },
+      loginInputs,
+      async (response) => {
+        if (response.status === 200) {
+          // Successful login
+          const data = await response.json();
+          // Store the authentication token securely (you may want to use a more secure storage method)
+          document.cookie = `authToken=${data.token}; Secure; HttpOnly`;
+          console.log('Login successful!');
+        } else {
+          // Handle login failure
+          console.error('Login failed!');
+        }
+      }
     );
+  };
+
+  const handleRegistration = async () => {
+    // Make the registration API call
+    apiCall(
+      '/auth/register',
+      'POST',
+      { 'Content-Type': 'application/json' },
+      registerInputs,
+      async (response) => {
+        if (response.status === 201) {
+          // Successful registration
+          const data = await response.json();
+          // Store the authentication token securely (you may want to use a more secure storage method)
+          document.cookie = `authToken=${data.token}; Secure; HttpOnly`;
+          console.log('Registration successful!');
+        } else {
+          // Handle registration failure
+          console.error('Registration failed!');
+        }
+      }
+    );
+  };
+
+  return (
+    <>
+      <h1>LoginPage</h1>
+      <div className="form-container">
+        <div className="login-btn-container">
+          <button className={'btn'} onClick={() => setHasAccount(true)}>
+            Log in
+          </button>
+          <button className={'btn'} onClick={() => setHasAccount(false)}>
+            Register
+          </button>
+        </div>
+
+        {hasAccount ? (
+          <LoginForm
+            inputs={loginInputs}
+            handleChange={(e) =>
+              setLoginInputs((prevState) => ({
+                ...prevState,
+                [e.target.name]: e.target.value.trim(),
+              }))
+            }
+            onSubmit={handleLogin}
+            submitType="Login"
+          />
+        ) : (
+          <LoginForm
+            inputs={registerInputs}
+            handleChange={(e) =>
+              setRegisterInputs((prevState) => ({
+                ...prevState,
+                [e.target.name]: e.target.value.trim(),
+              }))
+            }
+            onSubmit={handleRegistration}
+            submitType="Registration"
+          />
+        )}
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;
