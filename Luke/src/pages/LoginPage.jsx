4f20e39bfa043843ba241e9b6170a959c4a82453
Luke Slautterback <lukeslautterback@gmail.com>
2023-11-22 18:44:52 -0500

    Update

--- Luke/src/pages/LoginPage.jsx
+++ Luke/src/pages/LoginPage.jsx
@@ -28,7 +28,7 @@ function LoginPage({ apiCall }) {
         if (response.status === 200) {
           // Successful login
           const data = await response.json();
-          // Store the authentication token securely (you may want to use a more secure storage method)
+          // Store the authentication token securely
           document.cookie = `authToken=${data.token}; Secure; HttpOnly`;
           console.log('Login successful!');
         } else {
@@ -50,7 +50,7 @@ function LoginPage({ apiCall }) {
         if (response.status === 201) {
           // Successful registration
           const data = await response.json();
-          // Store the authentication token securely (you may want to use a more secure storage method)
+          // Store the authentication token securely
           document.cookie = `authToken=${data.token}; Secure; HttpOnly`;
           console.log('Registration successful!');
         } else {
